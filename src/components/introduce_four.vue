c9739b6471ff685e8356f4cb42d1048108e71a5c
曾智阳 <zzy18218203446@163.com>
2022-08-20 23:48:19 +0800

    feature: 工作室介绍页

---
+++ src/components/introduce_four.vue
@@ -0,0 +1,219 @@
+<script setup lang="ts">
+import { ref,onMounted  } from 'vue'
+ 
+//  声明变量获取refsDOM元素
+const one = ref()
+const two = ref()
+const three = ref()
+const four = ref()
+
+ 
+// 注意，这里如果不在挂载后输出到控制台，控制台是看不到dom元素的
+onMounted(() => {
+    let num = ['one','two','three','four'];
+    setInterval(() => {
+        //把数组的最后一个添加到第一个
+        num.unshift(num[3]);
+        //删除最后一个
+        num.pop();
+        one.value.className = num[0]
+        two.value.className = num[1]
+        three.value.className = num[2]
+        four.value.className = num[3]
+    }, 2000);
+
+
+})
+
+
+    
+
+
+</script>
+
+
+<template>
+  <div id="introducefour">
+    <!-- 外部的大盒子 -->
+    <div class="main">
+        <!-- 轮播的图片 -->
+        <ul class="imgs" ref="imgs">
+            <li class="one" ref="one">
+                <a href="javascript:;"><img src="../assets/cat1.jpg" alt=""></a>
+            </li>
+            <li class="two" ref="two">
+                <a href="javascript:;"><img src="../assets/cat2.jpg" alt=""></a>
+            </li>
+            <li class="three" ref="three">
+                <a href="javascript:;"><img src="../assets/cat3.jpg" alt=""></a>
+            </li>
+            <li class="four" ref="four">
+                <a href="javascript:;"><img src="../assets/cat4.jpg" alt=""></a>
+            </li>
+        </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+* {
+    margin: 0;
+    padding: 0;
+}
+
+a {
+    text-decoration: none;
+}
+
+li {
+    list-style: none;
+}
+
+/* 修改图片的大小 */
+img {
+    width: 100%;
+    border-radius: 15px;
+}
+
+/* 修改外部大盒子的大小 */
+.main {
+    position: relative;
+    width: 900px;
+    height: 300px;
+    /* 使盒子居中 */
+    top: 20%;
+    left: 50%;
+    transform: translate(-50%,10%);
+}
+
+/* 让包裹所有图片的盒子处于外部盒子的中间 */
+.imgs {
+    position: absolute;
+    width: 650px;
+    height: 300px;
+    top: 0;
+    left: 50%;
+    transform: translate(-50%,0%);
+}
+
+/* 让所有图片都叠在一起 */
+.imgs li {
+    position: absolute;
+    width: 650px;
+    /* 此处过渡效果 */
+    transition: 0.5s;
+}
+
+/* 给左右两侧的按钮设置样式和定位到两端 */
+.main .leftBtn {
+    position: absolute;
+    font-size: 20px;
+    /* 使其变圆 */
+    width: 40px;
+    height: 40px;
+    border-radius: 20px;
+    /* 设置文字样式且使文字居中 */
+    color: white;
+    line-height: 36px;
+    text-align: center;
+    /* 设置背景且使背景半透明 */
+    background-color: black;
+    opacity: 0.2;
+    /* 使按钮位于左侧中央并设置显示层级 */
+    top: 50%;
+    left: 0;
+    z-index: 500;
+}
+
+.main .rightBtn {
+    position: absolute;
+    font-size: 20px;
+    /* 使其变圆 */
+    width: 40px;
+    height: 40px;
+    border-radius: 20px;
+    /* 设置文字样式且使文字居中 */
+    color: white;
+    line-height: 36px;
+    text-align: center;
+    /* 设置背景且使背景半透明 */
+    background-color: black;
+    opacity: 0.2;
+    /* 使按钮位于左侧中央并设置显示层级 */
+    top: 50%;
+    right: 0;
+    z-index: 500;
+}
+
+/* 给左右两侧按钮添加鼠标移入时的样式（文字变亮） */
+.leftBtn:hover {
+    opacity: 0.5;
+}
+
+.rightBtn:hover {
+    opacity: 0.5;
+}
+
+/* 把图片分散到两侧且两侧图片缩小 */
+.imgs .one {
+    transform: translateX(-195px) scale(0.8);
+    z-index: 1;
+}
+
+.imgs .three {
+    transform: translateX(195px) scale(0.8);
+    z-index: 1;
+}
+
+/* 中间图片显示层级最高 */
+.imgs .two {
+    z-index: 2;
+}
+
+/* 开始时不显示的图片隐藏在中间图片的后面 */
+.imgs .four,.five,.six {
+    transform: scale(0.8);
+}
+
+
+/* 添加小圆圈样式 */
+.circle {
+    position: absolute;
+    bottom: -25px;
+    left: 50%;
+    margin-left: -72px;
+    z-index: 250;
+}
+
+.circle li {
+    float: left;
+    background-color: rgb(230, 230, 230);
+    width: 10px;
+    height: 10px;
+    border-radius: 5px;
+    margin: 0 6px;
+    cursor: pointer;
+}
+
+/* 小圆圈被选中时改变的样式 */
+.circle .current {
+    background-color: rgb(236,65,65);
+}
+
+/* 左右两部用来实现点击切换的盒子 */
+.rightBox {
+    position: absolute;
+    right: 25px;
+    bottom: 30px;
+    width: 100px;
+    height: 240px;
+}
+
+.leftBox {
+    position: absolute;
+    left: 25px;
+    bottom: 30px;
+    width: 100px;
+    height: 240px;
+}
+</style>
---
+++ types/introduce.js
@@ -0,0 +1,153 @@
+window.addEventListener('DOMContentLoaded', function () {
+  let cursor = document.getElementById("cursor");
+  let close = document.getElementById("close");
+  let body = document.body;
+  let links = document.getElementsByTagName("a")
+
+
+
+  // Load iFrames on demand & remove after modal is closed to reduce weight & smooth out transitions
+
+
+  let cards = document.getElementsByClassName("inner");
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("mousemove", function (event) {
+      cursor.classList.add("active");
+    });
+    cards[i].addEventListener("mouseover", function (event) {
+      cursor.classList.add("active");
+    });
+
+    cards[i].addEventListener("mouseout", function (event) {
+      cursor.classList.remove("active");
+    });
+    cards[i].addEventListener(
+      "click",
+      function (i) {
+        body.classList.add("active");
+      }.bind(null, i)
+    );
+  }
+
+  // hover events for social links
+
+
+  for (link of links) {
+    link.addEventListener("mouseover", function (event) {
+      cursor.classList.add("linkhover");
+    });
+    link.addEventListener("mousemove", function (event) {
+      cursor.classList.add("linkhover");
+    });
+    link.addEventListener("mouseout", function (event) {
+      cursor.classList.remove("linkhover");
+    });
+  }
+
+  // Escape key option to exit active state
+
+  document.onkeydown = function (evt) {
+    evt = evt || window.event;
+    let isEscape = false;
+    if ("key" in evt) {
+      isEscape = evt.key === "Escape" || evt.key === "Esc";
+    } else {
+      isEscape = evt.keyCode === 27;
+    }
+    if (isEscape) {
+      body.classList.remove("active");
+      // setTimeout(() => {
+      //   iframe.setAttribute("src", "");
+      // }, 2000);
+    }
+  };
+
+  if (close) {
+    close.addEventListener("click", function (event) {
+      body.classList.remove("active");
+      // setTimeout(() => {
+      //   iframe.setAttribute("src", "");
+      // }, 2000);
+    });
+  }
+
+
+  gsap.set("#cursor", {
+    xPercent: -50,
+    yPercent: -50
+  });
+
+  const pos = {
+    x: window.innerWidth / 2,
+    y: window.innerHeight / 2
+  };
+  const mouse = {
+    x: pos.x,
+    y: pos.y
+  };
+  const speed = 0.35;
+
+  const xSet = gsap.quickSetter(cursor, "x", "px");
+  const ySet = gsap.quickSetter(cursor, "y", "px");
+
+  window.addEventListener("mousemove", (e) => {
+    mouse.x = e.x;
+    mouse.y = e.y;
+  });
+
+  gsap.ticker.add(() => {
+    const dt = 1.0 - Math.pow(1.0 - speed, gsap.ticker.deltaRatio());
+
+    pos.x += (mouse.x - pos.x) * dt;
+    pos.y += (mouse.y - pos.y) * dt;
+    xSet(pos.x);
+    ySet(pos.y);
+  });
+
+  Splitting();
+
+  // Individual section scroll progress
+
+  gsap.utils.toArray(".panel").forEach((section, index) => {
+    gsap.to(this, {
+      scrollTrigger: {
+        trigger: section,
+        start: "top 100%",
+        end: "bottom 25%",
+        scrub: 0,
+        onUpdate: (self) => {
+          section.style.setProperty("--progress", self.progress);
+        }
+      }
+    });
+  });
+
+  // Full page scroll progress
+
+  gsap.to("body", {
+    scrollTrigger: {
+      trigger: "body",
+      start: "top 100%",
+      end: "50% 50%",
+      scrub: 0,
+      onUpdate: (self) => {
+        body.style.setProperty("--progress", self.progress);
+      }
+    }
+  });
+
+
+  // Set a delay on Scrolltrigger recalculation to accommodate element transition times
+
+  function refresh() {
+    setTimeout(() => {
+      ScrollTrigger.refresh();
+    }, 2500);
+  }
+
+  window.addEventListener("resize", refresh);
+
+  // Pull out the preloader
+
+  body.classList.add("loaded");
+})
