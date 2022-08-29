ab289d29225882a09298b82c68356f38a2de2cc3
陈挺 <2689841215@qq.com>
2022-08-29 17:01:07 +0800

    style:使用原生axios

--- src/view/Booking/Booking.vue
+++ src/view/Booking/Booking.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-table ref="tableRef" row-key="date" :data="toRaw(tableData)" style="width: 100%;" @cell-click="btnClick">
+    <el-table ref="tableRef" row-key="date" :data="toRaw(tableData)" style="width: 100%;">
         <el-table-column prop="date" label="日期" sortable width="180" column-key="date" :filters="
             // [ { text: '2016-05-01', value: '2016-05-01' }]
             filteArray
@@ -7,7 +7,15 @@
         </el-table-column>
         <el-table-column prop="time" label="时间" width="150" />
         <el-table-column prop="availableNumber" label="可预约" width="150" style="text-align: center;" />
-        <booking-button ref="sunBtn" />
+        <el-table-column prop="tag" label="预约" width="100" :filters="[
+            { text: '可预约', value: '预约' },
+        ]" :filter-method="filterTag" filter-placement="bottom-end">
+            <template #default="scope">
+                <!-- <el-tag :type="scope.row.tag === 'true' ? '' : 'success'" disable-transitions>{{ scope.row.tag }}
+                </el-tag> -->
+                <booking-button ref="sunBtn" :scope="scope" :refresh='dataFill' />
+            </template>
+        </el-table-column>
     </el-table>
     <!-- <el-button @click="resetDateFilter" style="float:left ;margin-top: 10px;;">清除筛选</el-button> -->
 </template>
@@ -22,19 +30,18 @@ import { ref } from 'vue';
 import BookingButton from './BookingButton.vue'
 //表格点击事件
 const sunBtn = ref<any>(null)
-const btnClick = (row, column, event, cell) => {
-    if (column.label == '预约') {
-        //调用button的函数，传递该行的时间字符串为参数
-
-        sunBtn.value.bookSubmit(row.timeQuantum);
-        console.log(toRaw(event.querySelector("el-button")))
-    }
-    console.log(toRaw(row.timeQuantum))
-    console.log(toRaw(row))
-    console.log(toRaw(column))
-    console.log(toRaw(event))
-    console.log(toRaw(cell))
-}
+// const btnClick = (row, column, event, cell) => {
+//     if (column.label == '预约') {
+//         //调用button的函数，传递该行的时间字符串为参数
+//         sunBtn.value.bookSubmit(row.timeQuantum);
+//         console.log(toRaw(event.querySelector("el-button")))
+//     }
+//     console.log(toRaw(row.timeQuantum))
+//     console.log(toRaw(row))
+//     console.log(toRaw(column))
+//     console.log(toRaw(event))
+//     console.log(toRaw(cell))
+// }
 // 使用封装的axios
 // let BookedData = BookData({ 'version': '0' }).then((res) => {
 //     console.log(res)
@@ -52,37 +59,41 @@ interface User {
     availableNumber: number
     tag: string
 }
-
+//筛选函数
+const filterTag = (value: string, row: User) => {
+    return row.tag === value
+}
 //请求代码
 // 输入表格数据格式
 let tableData = ref([] as User[])
 let filteArray = ref([] as Array<object>)
-axios({
-    url: 'http://127.0.0.1:4523/m1/1473415-0-default/cat/reservation/number',
-    method: 'post',
-    params: {
-        version: '0'
-    }
-}).then(res => {
-    let dataArray: Array<User> = []
-    let data = res.data.obj
-    // 填充表格
-    for (let i: number in data) {
-        // 表格数据
-        let obj = data[i].timetable
-        obj.tag = '预约'
-        obj.date = data[i].timetable.timeQuantum.split(' ')[0]
-        obj.time = data[i].timetable.timeQuantum.split(' ')[1].substring(0, 5)
-        dataArray.push(data[i].timetable)
-        // 筛选数据
-        let filterObj = { text: '', value: '' }
-        filterObj.text = obj.date
-        filterObj.value = obj.date
-        filteArray.value.push(filterObj)
-    }
-    tableData.value = dataArray
-})
-
+const dataFill = () =>
+    axios({
+        url: 'http://127.0.0.1:4523/m1/1473415-0-default/cat/reservation/number',
+        method: 'post',
+        params: {
+            version: '0'
+        }
+    }).then(res => {
+        let dataArray: Array<User> = []
+        let data = res.data.obj
+        // 填充表格
+        for (let i: number in data) {
+            // 表格数据
+            let obj = data[i].timetable
+            obj.tag = '预约'
+            obj.date = data[i].timetable.timeQuantum.split(' ')[0]
+            obj.time = data[i].timetable.timeQuantum.split(' ')[1].substring(0, 5)
+            dataArray.push(data[i].timetable)
+            // 筛选数据
+            let filterObj = { text: '', value: '' }
+            filterObj.text = obj.date
+            filterObj.value = obj.date
+            filteArray.value.push(filterObj)
+        }
+        tableData.value = dataArray
+    })
+dataFill()
 //备用：一键解除筛选功能
 // const tableRef = ref<InstanceType<typeof ElTable>>()
 // const resetDateFilter = () => {
--- src/view/Booking/BookingButton.vue
+++ src/view/Booking/BookingButton.vue
@@ -1,45 +1,85 @@
 <template>
-    <el-table-column prop="tag" label="预约" width="100" :filters="[
-        { text: '可预约', value: '预约' },
-    ]" :filter-method="filterTag" filter-placement="bottom-end" @cell-click="bookSubmit">
-        <template #default="scope">
-            <!-- <el-tag :type="scope.row.tag === 'true' ? '' : 'success'" disable-transitions>{{ scope.row.tag }}
-                </el-tag> -->
-            <el-button ref="btn" size="small" type="primary" id="0" @btnclick="bookSubmit">{{ scope.row.tag }}
-            </el-button>
-        </template>
-    </el-table-column>
+    <el-button ref="btn" size="small" :type="btnType" id="0" @click="bookSubmit">{{ props.scope.row.tag }}
+    </el-button>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import { timeSubmit, handleError } from '../../request/requestApi'
+import { ref, defineProps } from 'vue'
+import { handleError } from '../../request/requestApi'
 // import { toRaw } from '@vue/reactivity';
+import axios from 'axios'
 
-interface User {
-    id: number
-    number: number
-    date: string
-    time: string
-    version: number
-    availableNumber: number
-    tag: string
-}
-//筛选函数
-const filterTag = (value: string, row: User) => {
-    return row.tag === value
-}
+let props = defineProps({
+    // 接受按钮所在行的数据
+    scope: {
+        type: Object,
+        default: {}
+    },
+    refresh: {
+        type: Function,
+        default: function () { }
+    }
+})
 
 //按钮样式
-// const buttonType = ref('primary')
+const btnType = ref('primary')
 
 // 上传函数
-const bookSubmit = (time, btn) => {
-    console.log(time);
-    console.log(btn);
-    timeSubmit({ time: time }).then(res => {
-        console.log(res);
-    }).catch(handleError)
+let last = 0
+const bookSubmit = () => {
+    // 节流
+    let now = new Date().getTime()
+    if (now - last < 2000) {
+        return
+    }
+    // 判断当前状态
+    if (props.scope.row.tag == '预约') {
+        btnType.value = 'info'
+        props.scope.row.tag = '预约中'
+        axios({
+            url: 'http://127.0.0.1:4523/m1/1473415-0-default/cat/reservation/reservation/add',
+            method: 'post',
+            params: {
+                time: props.scope.row.timeQuantum
+            }
+        }).then(res => {
+            res
+            btnType.value = 'success'
+            props.scope.row.tag = '预约成功'
+            setTimeout(() => {
+                btnType.value = 'danger'
+                props.scope.row.tag = '取消预约'
+            }, 1500);
+            // 更新表格
+            props.refresh
+        }).catch(handleError)
+    }
+    if (props.scope.row.tag == '取消预约') {
+        btnType.value = 'info'
+        props.scope.row.tag = '取消ing'
+        axios({
+            url: 'http://127.0.0.1:4523/m1/1473415-0-default/cat/reservation/reservation/del',
+            method: 'post',
+            params: {
+                time: props.scope.row.timeQuantum
+            }
+        }).then(res => {
+            console.log(res);
+            btnType.value = 'warning'
+            props.scope.row.tag = '取消成功'
+            setTimeout(() => {
+                btnType.value = 'primary'
+                props.scope.row.tag = '预约'
+            }, 1500);
+            // 更新表格
+            props.refresh()
+        }).catch(handleError)
+    }
+    //封装使用
+    // timeSubmit({ time: props.scope.row.timeQuantum }).then(res => {
+    //     console.log(res);
+    // }).catch(handleError)
+    last = new Date().getTime()
 }
 
 const btn = ref<any>(null)
