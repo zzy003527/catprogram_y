0ee8a5b90f366b81ae00a20f6fd4476c4580c294
陈挺 <2689841215@qq.com>
2022-09-02 21:47:30 +0800

    fix：实现了将图片传到后端，但无法处理回调

--- src/views/aplication/ApplicationPhoto.vue
+++ src/views/aplication/ApplicationPhoto.vue
@@ -1,7 +1,7 @@
 <template>
     <el-form-item label="照片" prop="photo">
-        <el-upload ref="upload" class="upload-demo" :action='request.photoUpload' :limit="1" :on-exceed="handleExceed"
-            :auto-upload="false">
+        <el-upload ref="upload" class="upload-demo" :action='httpUrl.photoUpload' :limit="1" :on-exceed="handleExceed"
+            :auto-upload="false" :headers="headers" accept="image" :before-upload="beforeAvatarUpload">
             <template #trigger>
                 <el-button type="primary">选择照片</el-button>
             </template>
@@ -22,8 +22,15 @@
 import type { UploadInstance, UploadProps, UploadRawFile } from 'element-plus'
 import { ref } from 'vue'
 import { genFileId } from 'element-plus'
-import request from '../../request/httpUrl'
+import { ElMessageBox } from 'element-plus'
+import httpUrl from '../../request/httpUrl'
 // 照片上传部分
+// 获取token
+let token = localStorage.getItem("token")
+if (!token) {
+    token = sessionStorage.getItem("token")
+}
+let headers = { token: token }
 const upload = ref<UploadInstance>()
 
 const handleExceed: UploadProps['onExceed'] = (files) => {
@@ -36,6 +43,25 @@ const handleExceed: UploadProps['onExceed'] = (files) => {
 const submitUpload = () => {
     upload.value!.submit()
 }
+
+let beforeAvatarUpload = (File) => {
+    const isJPG = File.type === 'image/jpeg' || 'image/png'
+    const isLt2M = File.size / 1024 / 1024 < 2
+
+    if (!isJPG) {
+        ElMessageBox.alert('上传头像图片只能是 JPG 格式!', '提示', {
+            confirmButtonText: 'OK'
+        })
+    }
+    if (!isLt2M) {
+        ElMessageBox.alert('上传头像图片大小不能超过 2MB!', '提示', {
+            confirmButtonText: 'OK'
+        })
+    }
+    return isJPG && isLt2M
+}
+
+
 
 
 
