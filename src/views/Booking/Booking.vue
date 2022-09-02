f2d2337ff4014c03519d9f40ed1d9c4ce28c0bcb
陈挺 <2689841215@qq.com>
2022-09-02 18:53:47 +0800

    fix：修复了样式展示错乱的问题

--- src/views/Booking/Booking.vue
+++ src/views/Booking/Booking.vue
@@ -17,7 +17,7 @@
                 <template #default="scope">
                     <!-- <el-tag :type="scope.row.tag === 'true' ? '' : 'success'" disable-transitions>{{ scope.row.tag }}
                 </el-tag> -->
-                    <booking-button ref="sonBtn" :scope="scope" />
+                    <booking-button :scope="scope" />
                 </template>
             </el-table-column>
         </el-table>
@@ -44,7 +44,7 @@ const getCssVarName = (type: string) => {
 }
 // 失败弹窗
 const failLogin = (msg: string) => {
-  ElMessage.error(`${msg}`)
+    ElMessage.error(`${msg}`)
 }
 
 // element内置代码
@@ -66,75 +66,69 @@ interface User {
 const filterTag = (value: string, row: User) => {
     return row.tag === value
 }
-//获取子组件
-const sonBtn = ref()
 //请求代码
 //获取用户面试阶段
-
+// let version
+// // let btnType = ref('')
+// userProgress().then(res => {
+//     version = res.obj.testStatus
+// }).catch(handleError)
 // 输入表格数据格式
 let tableData = ref([] as User[])
 let filteArray = ref([] as Array<object>)
-
 // 获取预约数据
 function Book() {
-BookData({
-    version: store.state.nowTest
-}).then(res => {
-    if(res.resultStatus !== '200') {
-        failLogin(res.resultIns)
-    } else {
-        let dataArray: Array<User> = []
-    let data = res.obj
-    // let data = datause.obj
-    // 填充表格
-    filteArray.value = []
-    let filteArrayData: Array<string> = []
-    for (let i in data) {
-        // 表格数据
-        let obj = data[i].timetable
-        if (obj.availableNumber > 0) obj.tag = '预约'
-        else if (obj.availableNumber == 0) {
-            obj.tag = '已满'
+    BookData({
+        version: store.state.nowTest
+    }).then(res => {
+        if (res.resultStatus !== '200') {
+            failLogin(res.resultIns)
+        } else {
+            let dataArray: Array<User> = []
+            let data = res.obj
+            // let data = datause.obj
+            // 填充表格
+            filteArray.value = []
+            let filteArrayData: Array<string> = []
+            for (let i in data) {
+                // 表格数据
+                let obj = data[i].timetable
+                if (obj.availableNumber > 0) obj.tag = '预约'
+                else if (obj.availableNumber == 0) {
+                    obj.tag = '已满'
+                }
+                obj.date = data[i].timetable.timeQuantum.split(' ')[0].substring(0, 11)
+                obj.time = data[i].timetable.timeQuantum.split(' ')[0].substring(11, 19) + '-' + data[i].timetable.timeQuantum.split(' ')[0].substring(31, 39)
+                dataArray.push(data[i].timetable)
+                // 筛选数据
+                let filterObj = { text: '', value: '' }
+                filterObj.text = obj.date
+                filterObj.value = obj.date
+                //判断该时间是否已存在于筛选数组
+                if (!filteArrayData.includes(obj.date)) {
+                    filteArray.value.push(filterObj)
+                    filteArrayData.push(obj.date)
+                }
+            }
+            tableData.value = dataArray
         }
-
-        obj.date = data[i].timetable.timeQuantum.split(' ')[0].substring(0, 11)
-        obj.time = data[i].timetable.timeQuantum.split(' ')[0].substring(11, 19) + '-' + data[i].timetable.timeQuantum.split(' ')[0].substring(31, 39)
-        dataArray.push(data[i].timetable)
-        // 筛选数据
-        let filterObj = { text: '', value: '' }
-        filterObj.text = obj.date
-        filterObj.value = obj.date
-        //判断该时间是否已存在于筛选数组
-        if (!filteArrayData.includes(obj.date)) {
-            filteArray.value.push(filterObj)
-            filteArrayData.push(obj.date)
-        }
-    }
-    tableData.value = dataArray
-    }
-}).catch(handleError)
+    }).catch(handleError)
 }
 
 onMounted(() => {
     // 获取用户当前面试阶段
     userProgress().then(res => {
-    store.commit("confignowTest",res.obj.testStatus)
-    // 获取预约数据
-    Book()
-}).catch(handleError)
-
+        store.commit("confignowTest", res.obj.testStatus)
+        // 获取预约数据
+        Book()
+    }).catch(handleError)
 })
-
-
-
-
 //备用：一键解除筛选功能
 // const tableRef = ref<InstanceType<typeof ElTable>>()
 // const resetDateFilter = () => {
 //     tableRef.value!.clearFilter(['date'])
 // }
 // TODO: improvement typing when refactor table
-
 //筛选函数
 const filterHandler = (
     value: string,
@@ -152,22 +146,5 @@ const filterHandler = (
     border-radius: 15px;
     box-shadow: 33px;
     background-color: white;
-}
-
-.el-button:focus {
-    color: #ffffff;
-    background-color: #409eff;
-    outline: 0;
-}
-
-.el-button:hover {
-    color: #ffffff;
-    background-color: #79bbff;
-}
-
-.el-button:active {
-    color: #ffffff;
-    border-color: #409eff;
-    outline: 0;
 }
 </style>
--- src/views/Booking/BookingButton.vue
+++ src/views/Booking/BookingButton.vue
@@ -41,10 +41,10 @@ const bookSubmit = () => {
             time: props.scope.row.timeQuantum
         }
         console.log(params);
-        
+
         timeSubmit(params).then(res => {
             console.log(res);
-            
+
             if (res.resultStatus !== '200') {
                 ElMessageBox.alert(res.resultIns, '提示', {
                     // if you want to disable its autofocus
@@ -57,7 +57,7 @@ const bookSubmit = () => {
                 return 0
             } else {
                 ElMessageBox.alert('预约成功', '提示', {
-                confirmButtonText: 'OK',
+                    confirmButtonText: 'OK',
                 })
                 btnType.value = 'danger'
                 props.scope.row.tag = '取消预约'
@@ -98,20 +98,4 @@ defineExpose({
 </script>
 
 <style scoped>
-.el-button:focus {
-    color: #ffffff;
-    background-color: #409eff;
-    outline: 0;
-}
-
-.el-button:hover {
-    color: #ffffff;
-    background-color: #79bbff;
-}
-
-.el-button:active {
-    color: #ffffff;
-    border-color: #409eff;
-    outline: 0;
-}
 </style>
