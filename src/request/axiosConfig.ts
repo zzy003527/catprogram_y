7f731466d002cb91cc6f8c9bda3f951f4b5912eb
陈挺 <2689841215@qq.com>
2022-09-06 15:25:02 +0800

    fix：解决了token没有从session获取的问题

--- src/request/axiosConfig.ts
+++ src/request/axiosConfig.ts
@@ -8,14 +8,16 @@
  */
 //这是axios配置的ts文件 
 
-import axios, { AxiosRequestConfig,AxiosResponse } from 'axios';
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 // import axios from "axios"
 
 // 获取token
 let token = localStorage.getItem("token")
-    if(!token) {
-        token = sessionStorage.getItem("token")
-    }
+if (!token) {
+    token = sessionStorage.getItem("token")
+}
+
+
 
 // 创建axios的实例
 const service = axios.create({
@@ -31,15 +33,50 @@ const service = axios.create({
 // 添加请求拦截器
 service.interceptors.request.use((config: AxiosRequestConfig) => {
     let userToken = localStorage.getItem("token")
+    if (!userToken) {
+        userToken = sessionStorage.getItem("token")
+    }
     if (userToken) { //判断token是否存在
         config.headers!.token = userToken;  //将token设置成请求头
     }
+    console.log(userToken);
+    console.log(111111);
     return config;
 },
     (err: any) => {
         return Promise.reject(err)
     },
 )
+// service.interceptors.response.use(
+//     async response => {
+//       const config = response.config ;
+//       const resData = response.data;
+//       if (resData?.code === REQUEST_SUCCESS_CODE) {
+//         // 请求成功
+//         return Promise.resolve(resData?.data);
+//       }
+
+//       // token过期
+//       if (resData.subCode === 'user.token.expired') {
+//           // 重新请求token
+//           const authCode = await getAuthCodePromise();
+//           const [err, res] = await authBase(authCode);
+//           alipayUserId = res?.alipayUserId || '';
+//           userToken = res?.userToken || '';
+//           config.headers.userToken = userToken;
+//           // 重置baseUrl  特别注意：如果不重置会出现/api/api
+//           config.baseURL = '';
+//           // 重新请求
+//           return miniAxios(config);
+
+//       }
+//       // 错误处理
+//       return Promise.reject(resData);
+//     },
+//     err => {
+//     },
+//   );
+
 
 
 // //添加响应拦截器
