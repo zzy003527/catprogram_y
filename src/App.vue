6ff71cb124d8b03093189bdf88c4ad9ed17cbfe1
陈挺 <2689841215@qq.com>
2022-09-06 20:48:28 +0800

    fix：介绍页显示导航栏

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
+import navigation from './views/navigation/navigation.vue';
 
 </script>
 
 
 <template>
   <div id="app">
+    <navigation />
     <router-view></router-view>
   </div>
 </template>
